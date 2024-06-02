bea524075f7cdc99ba780e106f9bd20633b3d810
Nguyen Phi Thang <pthang769@gmail.com>
2024-06-02 20:25:23 +0900

    [Update] Update test and revoke key

--- test/google_translator_test.dart
+++ test/google_translator_test.dart
@@ -4,18 +4,6 @@ import 'package:google_translator/google_translator.dart';
 import 'package:google_translator/src/repository/google_translator_exception.dart';
 
 void main() {
-  test('adds one to input values', () async {
-    final googleTranslator = GoogleTranslator();
-    googleTranslator.setupAPIKey("AIzaSyCFZPwxguzEe8MzCSh-QYDZrqBxI6BhD9Q");
-    String text = await googleTranslator.simpleTextTranslate(
-      inputText: "Hello",
-      sourceLanguage: "en",
-      targetLanguage: "vi",
-    );
-
-    expect(text, "Xin chào");
-  });
-
   test('run without API key define', () async {
     final googleTranslator = GoogleTranslator();
 
